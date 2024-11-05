e76348333d18f422968af93e124fb860139677de
ganeshtaru <59159860+ganeshtaru@users.noreply.github.com>
2024-11-05 13:30:07 +0530

    Update NavHeader.js
    updated Português lang

--- mock-relying-party-ui/src/components/NavHeader.js
+++ mock-relying-party-ui/src/components/NavHeader.js
@@ -16,7 +16,7 @@ export default function NavHeader({ langOptions, i18nKeyPrefix = "background" })
   const fallbackLang =
     fallbackLangObj !== ""
       ? JSON.parse(fallbackLangObj)
-      : { label: "English", value: "en" };
+      : { label: "Português", value: "pt" };
 
   const changeLanguageHandler = (e) => {
     i18n.changeLanguage(e.value);
