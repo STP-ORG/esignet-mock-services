eef6eb0090efa582be46be75490d619ead0e6a0b
ganeshtaru <59159860+ganeshtaru@users.noreply.github.com>
2024-11-05 13:28:27 +0530

    Update env-config.js
    updated Português lang

--- mock-relying-party-ui/public/env-config.js
+++ mock-relying-party-ui/public/env-config.js
@@ -15,7 +15,7 @@ window._env_ = {
   PROMPT: "consent",
   GRANT_TYPE: "authorization_code",
   MAX_AGE: 21,
-  CLAIMS_LOCALES: "en",
-  DEFAULT_LANG: "en",
-  FALLBACK_LANG: "%7B%22label%22%3A%22English%22%2C%22value%22%3A%22en%22%7D"
+  CLAIMS_LOCALES: "pt",
+  DEFAULT_LANG: "pt",
+  FALLBACK_LANG: "%7B%22label%22%3A%22Português%22%2C%22value%22%3A%22pt%22%7D"
 };
